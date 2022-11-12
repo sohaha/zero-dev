e4634bbf6b6eb6033853664d1d8916bba7093eee
seekwe <seekwe@gmail.com>
2022-11-12 22:31:35 +0800

    --

--- internal/model/manage_restapi.go
+++ internal/model/manage_restapi.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sohaha/zlsgo/zlog"
 	"github.com/sohaha/zlsgo/znet"
 	"github.com/sohaha/zlsgo/ztype"
+	"github.com/sohaha/zlsgo/zutil"
 	"github.com/zlsgo/zdb"
 )
 
@@ -30,6 +31,7 @@ func (h *ManageRestApi) Init(g *znet.Engine) {
 	zerror.Panic(h.App.Di.Resolve(&h.db))
 	_ = g.GET("", h.lists)
 	_ = g.GET("/:model", h.info)
+	_ = g.GET("/:model/:view", h.view)
 }
 
 // lists 获取模型列表
@@ -81,4 +83,42 @@ func (h *ManageRestApi) info(c *znet.Context) error {
 	c.Byte(200, data)
 	c.SetContentType(znet.ContentTypeJSON)
 	return nil
+}
+
+// view 获取模型页面配置
+func (h *ManageRestApi) view(c *znet.Context) (interface{}, error) {
+	modelName := c.GetParam("model")
+	m, ok := globalModels.Get(modelName)
+	if !ok {
+		return nil, error_code.InvalidInput.Text("模型不存在")
+	}
+
+	view := c.GetParam("view")
+	data, ok := m.Views[view]
+	if !ok {
+		return nil, error_code.InvalidInput.Text("视图不存在")
+	}
+	columns := make([]ztype.Map, 0)
+
+	for _, v := range data.Fields {
+		column, ok := zarray.Find(m.Columns, func(_ int, c *Column) bool {
+			return c.Name == v
+		})
+		if !ok {
+			// 检查是不是内置字段
+			continue
+		}
+		columns = append(columns, ztype.Map{
+			"title": column.Label,
+			"key":   column.Name,
+		})
+		zlog.Debug(v, column)
+	}
+	info := ztype.Map{
+		"model":   m.Name,
+		"title":   zutil.IfVal(data.Title != "", data.Title, m.Name+"数据"),
+		"columns": columns,
+	}
+	zlog.Debug(data)
+	return info, nil
 }
