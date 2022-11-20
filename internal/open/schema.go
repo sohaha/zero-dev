05d1254ad1b958d240d8bd6ec8413b8c58e0748a
seekwe <seekwe@gmail.com>
2022-11-20 13:24:01 +0800

    --

--- internal/open/schema.go
+++ internal/open/schema.go
@@ -1,10 +1,12 @@
 package open
 
 import (
+	"zlsapp/internal/parse/jsonschema"
+
 	"github.com/sohaha/zlsgo/znet"
 )
 
 func (h *Open) GetSchemaModel(c *znet.Context) {
-	// c.Byte(200, parse.GetModelSchema())
+	c.Byte(200, jsonschema.GetModelSchema())
 	c.SetContentType(znet.ContentTypeJSON)
 }
